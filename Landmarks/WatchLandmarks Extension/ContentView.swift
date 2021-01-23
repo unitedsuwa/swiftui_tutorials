6f4e1bc95b3a5265a023065db41b90ca71d39144
suwa <unitedsuwa@gmail.com>
2021-01-23 18:26:28 +0900

    finished interfacing with UIkit

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by konstellation on 2021/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
