6ff3eb5fd84bd948447abd5d9d2a28abb11498b5
suwa <unitedsuwa@gmail.com>
2021-01-24 15:10:53 +0900

    finished Create watchOS App

--- Landmarks/WatchLandmarks Extension/ContentView.swift
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -1,21 +1,14 @@
-//
-//  ContentView.swift
-//  WatchLandmarks Extension
-//
-//  Created by konstellation on 2021/01/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
